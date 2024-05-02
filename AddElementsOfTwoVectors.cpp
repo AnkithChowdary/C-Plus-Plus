64a9cbfee94b7560106917d8e058e08e214ad98b
Ankith <ankithc29@gmail.com>
2024-05-02 22:16:55 +0530

    Update

--- AddElementsOfTwoVectors.cpp
+++ AddElementsOfTwoVectors.cpp
@@ -5,7 +5,16 @@
 using namespace std;
 
 int main(){
-    vector<int> v = {1, 6, 4, 7, 10, 11};
+
+    vector<int>v;
+    cout<<v.capacity()<<endl;
+    cout<<v.size()<<endl;
+    v.push_back(10);
+    cout<<v.size()<<endl;
+    cout<<v.capacity()<<endl;
+    v = {1, 6, 4, 7, 10, 11};
+      cout<<"Size:"<<v.size()<<endl;
+    cout<<"Capacity:"<<v.capacity()<<endl;
     vector<int> v1 = {1, 6, 4, 7, 1, 10};
 
     cout<<accumulate(v.begin(),v.end(),0);
