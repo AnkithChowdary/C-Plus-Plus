e265dee7803c51341aff28c8dbff7163dfaf812f
AnkithChowdary <ankithc29@gmail.com>
2024-06-28 23:27:04 +0530

    Update

--- DecimalToBinary.cpp
+++ DecimalToBinary.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
 using namespace std;
+int BtoD(String n){
+    int r=0;
+    int i=0;
+    while(n>0){
+        int bit=n%10;
+        r=r+bit*(1<<i);
+        n=n/10;
+        i++;
+    }
+    return r;
+}
 int DtoB(int n){
     int r=0;
     int i=0;
