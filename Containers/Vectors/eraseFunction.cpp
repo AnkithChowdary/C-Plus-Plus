28cdb197f7870d811d95bb3f617ea1711de0b37e
AnkithChowdary <ankithc29@gmail.com>
2024-07-06 18:35:02 +0530

    Update

---
+++ Containers/Vectors/eraseFunction.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int main() {
+
+  vector<int>v;
+  int i=4;
+  int s;
+  while(i>=0){
+    cout << "Enter the number:";
+    cin>>s;
+    v.push_back(s);
+    i--;
+  }
+  string a="34";
+  v.erase(v.begin()+2);
+  vector<int>::iterator it = v.begin();
+
+  while(it != v.end()) {
+      cout << *it;
+      it++;
+  }
+}
