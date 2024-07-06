1a227ad04936516e9fb1c2b93fe09fb7f5690142
AnkithChowdary <ankithc29@gmail.com>
2024-07-06 18:02:36 +0530

    Update

---
+++ Containers/Vectors/vectors2d.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+   vector<vector<int>> v(4,vector<int>(4,0));
+   // vector with different number of columns
+
+   vector<vector<int>>vs(5);
+   vs[0]=vector<int>(2);
+   vs[0]=vector<int>(3);
+   int rows=vs.size();
+   int columns=vs[0].size();
+   
+  
+  
+}
