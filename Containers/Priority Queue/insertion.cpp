4e29a8a614bc3c8a3d72ca1fa2944ccda76d4872
AnkithChowdary <ankithc29@gmail.com>
2024-07-07 22:29:26 +0530

    Update

---
+++ Containers/Priority Queue/insertion.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main() {
+  //Here elements are stored in max heap order (element with higheest value will be at the top)
+  priority_queue<int>pq;
+  pq.push(5);
+  pq.push(50);
+  pq.push(10);
+  cout << pq.top();  // 50 gets printed
+  return 0;
+}
