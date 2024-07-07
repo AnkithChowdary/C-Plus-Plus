56df613e27d84e400baab385c6202d011313f9f3
AnkithChowdary <ankithc29@gmail.com>
2024-07-08 01:02:54 +0530

    Update

--- Containers/Map/insertion.cpp
+++ Containers/Map/insertion.cpp
@@ -9,7 +9,7 @@ int main() {
   unordered_map<string, string>mp;
   mp.insert(make_pair("key1","value1"));
   mp.insert(make_pair("key2","value2"));
-  // unordered_map<string,string>::iterator it=mp.begin();
+  
   //Insertion using index
   mp["Name"]="Ankith";
   //Insertion by creating pairs and then adding to the map
@@ -22,9 +22,17 @@ int main() {
   cout << mp.at("Name1") << endl;
   //Updation at key
   mp.at("Name1")="Bhomraj";
-  cout << mp.at("Name1");
+  cout << mp.at("Name1") <<endl;
 
   // we can even use the indexing way to modify the value of a specific key
 
+
+  //Iterating map using iterator
+  unordered_map<string,string>::iterator it=mp.begin();
+  while (it!=mp.end()) {
+    pair<string,string>p=*it;
+    cout << p.first << " " << p.second <<endl;
+    it++;
+  }
   return 0;
 }
