d7ae9b9c1de0bc7b0963d26eafe895cdfd86f5f5
AnkithChowdary <ankithc29@gmail.com>
2024-07-08 12:40:25 +0530

    Update

---
+++ Algorithms/Iterator.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+
+void printDouble(int n) {
+    cout << n*2 << endl;
+}
+
+int main() {
+  vector<int>v;
+  v.push_back(2);
+  v.push_back(4);
+  v.push_back(10);
+  // This helps to perform  a function on a set of data
+  for_each(v.begin(),v.begin()+2,printDouble);    //Output: 4  8 
+}
