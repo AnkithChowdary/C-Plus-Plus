5e7817a73b6c14f2d42b5ba0153c7e4e94d2677d
AnkithChowdary <ankithc29@gmail.com>
2024-07-11 14:33:35 +0530

    Update

--- Searching and Sorting/binarySearch.cpp
+++ Searching and Sorting/binarySearch.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int search(vector<int>& nums, int target) {
         int start=0,end=nums.size()-1;
     while(start<=end){
-        int mid=(start+end)/2;
+        int mid=start+(end-start)/2;
         if(target<nums[mid])
             end=mid-1;
         else if(target==nums[mid])
---
+++ Searching and Sorting/findingFirstOccurrence.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void firstOccurrence(vector<int>arr,int target,int &index) {
+  int start = 0, end = arr.size()-1;
+  int mid = start + (end-start)/2;
+  while(start <= end) {
+    if(target < arr[mid])
+      end = mid-1;
+    else if(target == arr[mid]){
+      index = mid;
+     end = mid-1;
+    }
+     else
+     start = mid+1;
+
+mid = start + (end-start)/2;
+     
+  }
+
+}
+int main(){
+    vector<int>arr(5);
+    arr[0]=4;
+    arr[1]=4;
+    arr[2]=7;
+    arr[3]=9;
+    arr[4]=11;
+  int index=-1;
+    firstOccurrence(arr,4,index);   //Output : 0
+
+    cout << index;
+    return 0;
+}
