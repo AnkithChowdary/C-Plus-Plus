5f1cbaf265aa30dda857ca20d564ea2b3de9a3e0
AnkithChowdary <ankithc29@gmail.com>
2024-07-13 16:52:55 +0530

    Update

---
+++ Arrays/patterns.cpp/pattern3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+  int arr[]={10,20,30,40,50};
+  for(int i=0;i<5;i++){
+    for(int j=5-1;j>=0;j--){
+      cout << arr[i] << "    " << arr[j] << endl;
+    }
+  }
+}
