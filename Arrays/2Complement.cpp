a664fa9f4acdbe1a7c1090d7a11d62d6a07681f1
AnkithChowdary <ankithc29@gmail.com>
2024-07-13 23:13:26 +0530

    Update

--- Arrays/2Complement.cpp
+++ Arrays/2Complement.cpp
@@ -3,29 +3,29 @@
 using namespace std;
 
 vector<int> twosComplement(vector<int> v) {
-    vector<int> compli(v.size(), 0);
+    vector<int> comple(v.size()+1, 0);
 
     // Step 1: Inverting the bits
-    for (int i = v.size()-1; i >=0; i--) {
-        compli[i] = v[i] == 0 ? 1 : 0;
+    for (int i = comple.size()-1,k=v.size()-1; i >=0; i--,k--) {
+        comple[i] = v[k] == 0 ? 1 : 0;
     }
 
     // Step 2: Adding 1 to the inverted bits
     int carry = 1;
-    for (int i = compli.size() - 1; i >= 0; i--) {
-        int sum = compli[i] + carry;
-        compli[i] = sum % 2;
+    for (int i = comple.size() - 1; i >= 0; i--) {
+        int sum = comple[i] + carry;
+        comple[i] = sum % 2;
         carry = sum / 2;
     }
     if(carry){
-      compli[0]=carry;
+      comple[0]=carry;
     }
 
-    return compli;
+    return comple;
 }
 
 int main() {
-    vector<int> v = {0, 0, 1, 0};
+    vector<int> v = {0, 0, 0, 0};
     vector<int> re = twosComplement(v);
 
     // Printing the array
