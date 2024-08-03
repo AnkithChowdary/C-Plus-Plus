15475c24e883f5b70083b1ad4d12a35021b7deb5
AnkithChowdary <ankithc29@gmail.com>
2024-08-03 19:35:00 +0530

    Update

--- Maths/SeiveOfEratosthenes.cpp
+++ Maths/SeiveOfEratosthenes.cpp
@@ -1,51 +1,69 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
 
-void isPrime(int n,vector<bool>&v){
-  v[0]=v[1]=false;
-  for(int i=2;i<=n;i++){
-    if(v[i]){
-    int j=2*i;
-    while(j<=n){
-      v[j]=false;
-      j+=i;
-    }
-    }
-  }
-}
-// Optimized Code
-void isPrime1(int n,vector<bool>&v){
-    v[0]=v[1]=false;
-    for(int i=2;i<=n;i++){
-      if(v[i]){
-        // starts from i*i as others are already marked from 2 to i-1
-        int j=i*i;
-        while(j<=n){
-          v[j]=false;
-          j+=i;
+void isPrime(int n, vector<bool>& v) {
+    v[0] = v[1] = false;
+    for (int i = 2; i <= n; i++) {
+        if (v[i]) {
+            int j = 2 * i;
+            while (j <= n) {
+                v[j] = false;
+                j += i;
+            }
         }
-      }
     }
 }
-  
-int main() {
-  int n;
-  cin >> n;
-  vector<bool>tprimes(n+1,true);
-  isPrime(n,tprimes);
-  for(int i=2;i<=n;i++){
-      if(tprimes[i]){
-        cout << i << " ";
-      }
-  }
-  cout << endl;
-  vector<bool>primes(n+1,true);
-  isPrime1(n,primes);
-  for(int i=2;i<=n;i++){
-      if(primes[i]){
-        cout << i << " ";
-      }
-  }
 
+// Optimized Code
+void isPrime1(int n, vector<bool>& v) {
+    v[0] = v[1] = false;
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (v[i]) {
+            // Starts from i*i as others are already marked from 2 to i-1
+            int j = i * i;
+            while (j <= n) {
+                v[j] = false;
+                j += i;
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<bool> tprimes(n + 1, true);
+    auto start = high_resolution_clock::now();
+    isPrime(n, tprimes);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by isPrime: " << duration.count() << " microseconds" << endl;
+    
+    for (int i = 2; i <= n; i++) {
+        if (tprimes[i]) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+    
+    vector<bool> primes(n + 1, true);
+    start = high_resolution_clock::now();
+    isPrime1(n, primes);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by isPrime1: " << duration.count() << " microseconds" << endl;
+    
+    for (int i = 2; i <= n; i++) {
+        if (primes[i]) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
 }
