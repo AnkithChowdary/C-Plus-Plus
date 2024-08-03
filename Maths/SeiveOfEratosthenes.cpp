8c78f30321c7c87090ac0c761c7294f666952d15
AnkithChowdary <ankithc29@gmail.com>
2024-08-03 19:00:56 +0530

    Update

---
+++ Maths/SeiveOfEratosthenes.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void isPrime(int n,vector<bool>&v){
+  v[0]=v[1]=false;
+  for(int i=2;i<=n;i++){
+    if(v[i]){
+    int j=2*i;
+    while(j<=n){
+      v[j]=false;
+      j+=i;
+    }
+    }
+  }
+}
+  
+int main() {
+  int n;
+  cin >> n;
+  vector<bool>tprimes(n+1,true);
+  isPrime(n,tprimes);
+  for(int i=2;i<=n;i++){
+      if(tprimes[i]){
+        cout << i << " ";
+      }
+  }
+
+}
