3f01485ff5f83049831f2171a240ebf899a5a6d4
Your Name <ankithchowdary25@gmail.com>
2025-06-17 12:31:45 +0530

    Add basic stack operations example in stackBasics.cpp
    - Implemented a simple program demonstrating stack operations using C++ STL.
    - Included operations: push, pop, size, empty, and top.
    - Output the results of these operations to the console.

---
+++ Patterns/numberHallowInvertedHalfPyramid.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin>>n;
+    for(int i=0;i<n;i++){
+      for(int j=i+1;j<=n;j++){
+        if(i+1==j||j==n)
+        cout<<j;
+        else
+        cout<<" ";
+      }
+      cout<<endl;
+
+    }
+    return 0;
+}
