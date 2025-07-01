d4a138b47ccc0dc1c6fa9f235dc0294ba8e397f0
Your Name <ankithchowdary25@gmail.com>
2025-07-01 15:28:25 +0530

    Add implementation for printing all subarrays in C++

---
+++ Recursion/subArrraysPrint.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+void printAllSub(vector<int>arr,int start,int end){
+  if(end==arr.size()){
+    return;
+  }
+
+  for(int i=start;i<=end;i++){
+    cout<< arr[i] << " ";
+  }
+  cout<<endl;
+  printAllSub(arr,start,end+1);
+}
+void printSub(vector<int>arr){
+  for(int start=0;start<arr.size();start++){
+     int end=start;
+     printAllSub(arr,start,end);
+  }
+}
+int main() {
+    vector<int>arr={1,5,7,9};
+    printSub(arr);
+    return 0;
+}
